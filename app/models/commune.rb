17e35583278795a1ac5742cabb2a5cb7cd6ad323
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2017-03-29 13:55:03 +0200

    Level 2: few models methods

--- app/models/commune.rb
+++ app/models/commune.rb
@@ -5,4 +5,12 @@ class Commune < ApplicationRecord
 
   validates :name, presence: true
   validates :code_insee, presence: true, length: { is: 5 }
+
+  def self.search(search)
+    where('LOWER(name) LIKE ?', "%#{sanitize_sql_like(search.mb_chars.downcase)}%")
+  end
+
+  def self.to_hash
+    all.map { |c| [(c.code_insee).to_s, c.name] }.to_h
+  end
 end
--- app/models/intercommunality.rb
+++ app/models/intercommunality.rb
@@ -4,4 +4,12 @@ class Intercommunality < ApplicationRecord
   validates :name, presence: true
   validates :siren, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A\d{9}\z/ }
   validates :form, inclusion: { in: %w(ca cu cc met) }
+
+  attribute :slug, :string
+
+  after_save { self.slug = self.name.parameterize if self.slug.nil? && !self.name.nil? }
+
+  def communes_hash
+    self.communes.map { |c| [(c.code_insee).to_s, c.name] }.to_h
+  end
 end
